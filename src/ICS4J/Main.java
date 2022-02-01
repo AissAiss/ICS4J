4fbb20793367aaf9c6478154ef285b2e5f79bf0e
AissAiss <lucasaissaoui201@gmail.com>
2022-02-01 15:10:58 +0100

    Modification des getteurs de la classe ADEEvents (Changement de nom et suppression des '\n', '\' et des balises en début de ligne)

--- src/ICS4J/Main.java
+++ src/ICS4J/Main.java
@@ -10,6 +10,13 @@ public class Main {
 		ADECalendar cal = ICSparser.getADECalendar(file);
 		cal.printAllEvents();
 		//cal.printAllDays();
+		//System.out.println(cal.getEvent(0).getSUMMARY());
+		//System.out.println(cal.getEvent(0).getSummary()); 
+		//System.out.println(cal.getEvent(0).getLOCATION());
+		//System.out.println(cal.getEvent(0).getLocation());
+		//System.out.println(cal.getEvent(1).getDESCRIPTION());
+		//System.out.println();
+		//System.out.println(cal.getEvent(1).getDescription());
 	}
 
 }
