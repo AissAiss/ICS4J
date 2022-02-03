e9717740d3f0fab2c336be1a75e573ae839d8b95
AissAiss <lucasaissaoui201@gmail.com>
2022-02-03 16:29:29 +0100

    Ajout de la méthode pour avaoir l'index du jour courant.

--- src/ICS4J/Main.java
+++ src/ICS4J/Main.java
@@ -1,22 +1,28 @@
 package ICS4J;
 import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
+	
+	public static void testDate() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
+		System.out.println(dtf.format(now));
+		int day = now.getDayOfMonth(); 
+		int month = now.getMonthValue(); 
+		int year = now.getYear(); 
+		
+		System.out.println(day + "/" + month + "/" + year); 
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		File file = new File("ADECal.ics"); 
 		ADECalendar cal = ICSparser.getADECalendar(file);
-		cal.printAllEvents();
-		//cal.printAllDays();
-		//System.out.println(cal.getEvent(0).getSUMMARY());
-		//System.out.println(cal.getEvent(0).getSummary()); 
-		//System.out.println(cal.getEvent(0).getLOCATION());
-		//System.out.println(cal.getEvent(0).getLocation());
-		//System.out.println(cal.getEvent(1).getDESCRIPTION());
-		//System.out.println();
-		//System.out.println(cal.getEvent(1).getDescription());
+		
+		System.out.println(cal.getDay(cal.getIndexOfCurentDay())); 
 	}
 
 }
